35e5639bd895348234ab3eab71f6c488e5bfd849
Carlos Kieliszewski <63735286+cryptikkUS@users.noreply.github.com>
2022-05-10 15:42:48 -0600

    Delete scratchclient directory

--- scratchclient/IncompleteUser.py
+++
@@ -1,7 +0,0 @@
-class IncompleteUser:
-    def __init__(self, data):
-        self.username = data["username"]
-        self.id = data["id"]
-        self.scratchteam = data["scratchteam"]
-        self.joinedTimestamp = data["history"]["joined"]
-        self.avatar_URL = data["profile"]["images"]["90x90"]
