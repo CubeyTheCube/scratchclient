445231031c295b701d1c7481564a052445817c6c
Carlos Kieliszewski <63735286+cryptikkUS@users.noreply.github.com>
2022-05-10 15:32:56 -0600

    Delete scratchclient directory

--- scratchclient/News.py
+++
@@ -1,8 +0,0 @@
-class News:
-    def __init__(self, data):
-        self.id = data["id"]
-        self.timestamp = data["stamp"]
-        self.title = data["headline"]
-        self.description = data["copy"]
-        self.image_URL = data["image"]
-        self.src = data["url"]
