2333b944dfc32adb72ee397a6b7d95cd5259f6c3
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-08-31 08:58:09 +0530

    New line and indent after brace

--- scratchclient/Scraping.py
+++ scratchclient/Scraping.py
@@ -402,7 +402,8 @@ class HTMLToBBCodeParser(HTMLParser):
 # For lack of a better name
 class ScrapingSession:
     def __init__(self, client):
-        self._headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36."}
+        self._headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36."}
         self._client = client
 
     def get_follower_count(self, user):
